93c14c26398b0c4773ffdc6ba7bae81f23b95398
Fayssaldagod <you@example.com>
2023-09-10 12:57:48 -0700

    test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,15 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Failure)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t written = write(STDERR_FILENO, message, sizeof(message) - 1); // Write to stderr (file descriptor 2)
-
-    if (written == -1) {
-        // Error occurred while writing
-        return 1;
-    }
-
-    return 1; // Return 1 as required
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
+
