c3ebbab80d283e9c7d81e68c7b4f024f5acadc7c
Fayssaldagod <you@example.com>
2023-09-11 14:05:00 -0700

    test

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,10 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Determines if a number is positive, negative or zero.
  *
- * Return : Always 0 (Success)
-*/
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
