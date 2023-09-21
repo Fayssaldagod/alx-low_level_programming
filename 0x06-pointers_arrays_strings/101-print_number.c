e81b7646e992195d46bee29220a595ceab69cc15
Fayssaldagod <you@example.com>
2023-09-21 14:48:09 -0700

    test

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
+ * Return: On success, returns the character written. On error, returns -1.
  */
-
-void print_number(int n)
+int _putchar(char c)
 {
-	unsigned int n1;
-
-	n1 = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n1 = -n;
-	}
-
-	if (n1 / 10 != 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+    return write(1, &c, 1);
 }
 
+/**
+ * print_number - Print numbers as characters
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+    unsigned int n1;
+
+    n1 = (n < 0) ? -n : n;
+
+    if (n < 0)
+        _putchar('-');
+
+    if (n1 / 10 != 0)
+        print_number(n1 / 10);
+
+    _putchar((n1 % 10) + '0');
+}
